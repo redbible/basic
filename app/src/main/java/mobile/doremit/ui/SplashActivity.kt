9232f501f9e550f7fbdbedd6f87f51a8e56144dc
redbible <sheiud123@gmail.com>
2019-02-12 23:12:31 +0900

    스플래쉬 구현

--- app/src/main/java/mobile/doremit/ui/SplashActivity.kt
+++ app/src/main/java/mobile/doremit/ui/SplashActivity.kt
@@ -3,6 +3,7 @@ package mobile.doremit.ui
 import coinone.co.kr.official.common.ui.activity.BaseActivity
 import io.reactivex.Observable
 import mobile.doremit.ActivityNavigator
+import mobile.doremit.BuildConfig
 import java.util.concurrent.TimeUnit
 
 class SplashActivity : BaseActivity(0) {
@@ -12,8 +13,14 @@ class SplashActivity : BaseActivity(0) {
     }
 
     override fun setupView() {
-        Observable.just(2)
-            .delay(2, TimeUnit.SECONDS)
-            .subscribe { ActivityNavigator.with(this).main().start() }
+        if (BuildConfig.DEBUG) {
+            ActivityNavigator.with(this).main().start()
+        } else {
+            Observable.just(0)
+                .delay(2, TimeUnit.SECONDS)
+                .subscribe { ActivityNavigator.with(this).main().start() }
+        }
+
+        finish()
     }
 }
