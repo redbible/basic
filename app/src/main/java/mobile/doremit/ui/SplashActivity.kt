bdbee582bef081e1af6e5eb2083138b0669a4ca8
redbible <sheiud123@gmail.com>
2019-02-12 22:53:53 +0900

    리소스 추가

--- app/src/main/java/mobile/doremit/ui/SplashActivity.kt
+++ app/src/main/java/mobile/doremit/ui/SplashActivity.kt
@@ -1,15 +1,19 @@
 package mobile.doremit.ui
 
 import coinone.co.kr.official.common.ui.activity.BaseActivity
+import io.reactivex.Observable
 import mobile.doremit.ActivityNavigator
+import java.util.concurrent.TimeUnit
 
-class SplashActivity: BaseActivity(0) {
+class SplashActivity : BaseActivity(0) {
 
     override fun setContentView() {
 
     }
 
     override fun setupView() {
-        ActivityNavigator.with(this).main().start()
+        Observable.just(2)
+            .delay(2, TimeUnit.SECONDS)
+            .subscribe { ActivityNavigator.with(this).main().start() }
     }
 }
